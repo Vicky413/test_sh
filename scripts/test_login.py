e74ffe35aa03b5bea25d4e747410cabbc20c66e0
Vicky <vicky3609@163.com>
2019-08-13 22:13:24 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,15 @@
+class TestLogin:
+
+    def test_login1(self):
+        print("123")
+        assert 1
+
+
+    def test_login2(self):
+        print("456")
+        assert 1
+
+
+    def test_login3(self):
+        print("789")
+        assert 0
